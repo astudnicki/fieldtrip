66023a4f384cadd3cd4443d9fa14f780a98bbd97
Amanda Studnicki <astudnicki@ufl.edu>
2021-06-17 18:37:49 -0400

    Test git

--- ft_defaults.m
+++ ft_defaults.m
@@ -1,4 +1,5 @@
 function ft_defaults
+%
 
 % FT_DEFAULTS (ending with "s") sets some general settings in the global variable
 % ft_default (without the "s") and takes care of the required path settings. You can
